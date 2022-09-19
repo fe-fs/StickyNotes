5beb0544cfa68566cc2060df734ca5da278baea9
Fernanda Silva <fe.fs@hotmail.com>
2022-09-19 10:26:10 -0400

    stick notes visual ready

---
+++ src/sticky.js
@@ -0,0 +1,30 @@
+//parei 14 min
+
+function getNotes(){
+    //retrieve all the existing notes from the client's browser
+}
+
+function saveNotes(notes){
+    //get an array of notes - save all the new notes to the local storage and the client's browser
+}
+
+function createNoteElement(id, content) {
+    //get an id for the note and the content of the note.
+    //build a new html element to represent a note
+
+
+}
+
+function addNote(){
+    //adding a new note to the html and will save it too, to the local storage
+}
+
+function updateNote(id, newContent){
+    //will update the note instead of add a new one
+}
+
+function deleteNote(id,element){
+    //get the id and the html element that represents this note and delete it
+}
+
+/*Local storage is an API that saves this info in a JSON file*/
